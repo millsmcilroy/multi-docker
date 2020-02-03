198e541d90a226ee2b1bdc5fa3be79339c46e1ba
millsmcilroy <37815163+millsmcilroy@users.noreply.github.com>
2020-02-03 13:45:57 -0700

    Change h1 to p

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,9 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Fib Calculator</h1>
+          <p>
+            Fib Calculator
+          </p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
