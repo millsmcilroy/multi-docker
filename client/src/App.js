19a6cc58305ce78deae47b4137b99d704ba6dc4b
millsmcilroy <37815163+millsmcilroy@users.noreply.github.com>
2020-02-09 18:38:41 -0700

    Update title

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Fib Calculator
+            Fib Calculator version 2
           </p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
